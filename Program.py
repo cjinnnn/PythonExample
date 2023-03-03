3171c61ec618ed6ab51a92c9d1ec06802e8ae799
cjinnnn <cjinnnn@naver.com>
2023-03-04 00:42:43 +0900

    윈도우용 중복실행 방지 추가

--- Program.py
+++ Program.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 import sys
+import platform
 from PyQt5.QtWidgets import QApplication
 
 from MainData import MainData
 from gui.MainFrame import Ui_MainFrame
 
-if __name__ == "__main__":
-    import fcntl
+def IsRunning():
     isRun = False
     pid_file = 'program.pid'
     fp = open(pid_file, 'w')
-    try:
-        fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except:
-        isRun = True
+    os_txt = platform.system()
+    if os_txt == "Linux":
+        import fcntl
+        try:
+            fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except:
+            isRun = True
+    elif os_txt == "Windows":
+        import portalocker
+        try:
+            portalocker.lock(fp, portalocker.LOCK_EX | portalocker.LOCK_NB)
+        except:
+            isRun = True
+    return isRun, fp
 
+if __name__ == "__main__":
+    isRun, fp= IsRunning()
     if isRun == False:
         app = QApplication(sys.argv)
         mainData = MainData.instance(app)
