4b9de09b575038379dcced49fdaf2de952f04372
cjinnnn <cjinnnn@naver.com>
2023-03-05 22:25:14 +0900

    페이지 추가.

---
+++ gui/pageThird.py
@@ -0,0 +1,43 @@
+import sys
+import os
+
+#단독 실행 할 때는 메인 경로 추가 해 줘야 함
+if __name__ == "__main__":
+    def append_path(path):
+        if path not in sys.path:
+            sys.path.append(path)
+    def get_top_dir_path(name):
+        path = os.path.dirname(__file__)
+        while True:
+            path = os.path.dirname(os.path.abspath(path))
+            dir_name = os.path.basename(path)
+            if dir_name == name:
+                break
+            elif  dir_name == '':
+                path = None
+        return path
+    def root_path_append(root_name):
+        path =  get_top_dir_path(root_name)
+        append_path(path)
+        append_path(os.path.dirname(__file__))
+    root_path_append("PythonExample")
+
+from typing import List
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QTimer, Qt
+from MainData import MainData
+
+class pageThird(QWidget):
+    def __init__(self):
+        super(pageThird, self).__init__() ## Call the inherited classes __init__ method
+        self.mainData = MainData.instance()
+        uic.loadUi(os.path.join(self.mainData.dir.GUI_DIR, "pageThird.ui"), self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainData = MainData.instance(app)
+    window = pageThird()
+    window.show()
+    app.exec_()
+    
